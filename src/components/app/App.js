0dd53b2c08827d0847b396411ba090f73c6fba35
Katherine Tam <katherinemtam@gmail.com>
2021-08-13 01:09:16 -0700

    add ThemeToggle to App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import BreedsList from '../breeds/BreedsList';
-import BreedToggle from '../breeds/BreedToggle';
+import BreedToggle from '../toggles/BreedToggle';
+import ThemeToggle from '../toggles/ThemeToggle';
 
 function App() {
   return (
-    <>
+    <div className="flex flex-col justify-center items-center mx-auto p-8 rounded-xl shadow-lg">
+      <ThemeToggle />
       <BreedsList />
-    </>
+    </div>
   );
 }
 
