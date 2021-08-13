e89fdc70f3dde0deb877abf4eade37507b8951cd
Katherine Tam <katherinemtam@gmail.com>
2021-08-13 09:43:40 -0700

    apply theme styling to App.js for theme toggle

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import BreedsList from '../breeds/BreedsList';
+import { useThemes } from '../state/ThemeProvider';
 import BreedToggle from '../toggles/BreedToggle';
 import ThemeToggle from '../toggles/ThemeToggle';
+import { theme } from '../../services/theme.js';
 
 function App() {
+  const { mode } = useThemes();
+
   return (
-    <div className="flex flex-col justify-center items-center mx-auto p-8 rounded-xl shadow-lg">
+    <div className={`flex flex-col justify-center items-center ${theme[mode]}`}>
+      <h1>The CatDog Encyclopedia</h1>
       <ThemeToggle />
       <BreedsList />
     </div>
