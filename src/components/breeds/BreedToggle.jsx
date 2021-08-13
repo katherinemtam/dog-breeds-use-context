a86c462ca7627e0bc3202d0917872d359e607ee9
Katherine Tam <katherinemtam@gmail.com>
2021-08-12 22:14:46 -0700

    dynamic rendering of breed toggle options

--- src/components/breeds/BreedToggle.jsx
+++ src/components/breeds/BreedToggle.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
-import { useSetSelectedApi } from '../state/BreedProvider';
+import { useAvailableAPIs, useSetSelectedApi } from '../state/BreedProvider';
 
 const BreedToggle = () => {
+  const availableAPIs = useAvailableAPIs()
   const setSelectedApi = useSetSelectedApi();
+  
 
   const handleChange = ({target}) => {
     setSelectedApi(target.value)
   }
 
   return(
-    <>
-      <input 
-        type="radio" 
-        name="api" 
-        value="cats" 
-        onChange={handleChange}
-      />
-      <input 
-        type="radio" 
-        name="api" 
-        value="dogs" 
-        onChange={handleChange}
-      />
-    </>
+    <ul>
+      {availableAPIs.map(api => (
+        <li key={api}>
+          <label>{api}</label>
+          <input 
+              type="radio" 
+              name="api" 
+              value={api}
+              onChange={handleChange}
+          />
+        </li>
+      ))}
+    </ul>
   )
 }
 
