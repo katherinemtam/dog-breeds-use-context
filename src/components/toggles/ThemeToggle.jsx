62d87fb550bb76636139c12a7ec894171a8c26dc
Katherine Tam <katherinemtam@gmail.com>
2021-08-13 09:42:58 -0700

    apply state name change to ThemeToggle

--- src/components/toggles/ThemeToggle.jsx
+++ src/components/toggles/ThemeToggle.jsx
@@ -3,7 +3,7 @@ import { useThemes } from '../state/ThemeProvider';
 import './ThemeToggle.css';
 
 const ThemeToggle = () => {
-  const { theme, setTheme } = useThemes();
+  const { mode, setTheme } = useThemes();
 
   return(
       <div className="flex items-center justify-center w-full mb-12">
@@ -12,7 +12,7 @@ const ThemeToggle = () => {
             Light Mode
           </div>
           <div className="relative">
-            <input type="checkbox" id="toggle" className="sr-only" checked={theme === "light" ? true : false} onChange={setTheme}/>
+            <input type="checkbox" id="toggle" className="sr-only" checked={mode === "light" ? true : false} onChange={setTheme}/>
             <div className="block bg-gray-600 w-14 h-8 rounded-full"></div>
             <div className="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
           </div>
