e9a6eb15887c474d87c4a1773b0b5fa9b0a3dd5e
Katherine Tam <katherinemtam@gmail.com>
2021-08-12 18:11:53 -0700

    BreedsList component

---
+++ src/components/breeds/BreedsList.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import { fetchDogs } from '../../services/dogApi';
+import { fetchCats } from '../../services/catApi'; 
+import Breed from './Breed';
+
+const BreedsList = () => {
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    fetchDogs()
+    .then(setBreeds);
+  }, [])
+
+  const breedElements = breeds.map(breed => (
+  <li key={breed.name}>
+    <Breed {...breed}/>
+  </li>
+  ))
+
+  return <ul>{breedElements}</ul>
+}
+
+export default BreedsList;
