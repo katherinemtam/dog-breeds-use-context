e081651d5f5c5f6070ce0ffb9dbac5ed4d319473
Katherine Tam <katherinemtam@gmail.com>
2021-08-12 20:06:45 -0700

    remove console.log

--- src/services/catApi.js
+++ src/services/catApi.js
@@ -1,9 +1,9 @@
 export const fetchCats = async() => {
   const res = await fetch(`https://api.thecatapi.com/v1/breeds`)
   const json = await res.json();
-  console.log(json)
+
   const mungedCats = json.map(cat => ({
-    name: cat.name || "Unknown",
+    name: cat.name,
     url: cat?.image?.url || "Image Unavailable",
     origin: cat.origin || "Unknown",
     lifespan: cat.life_span || "Unknown"
